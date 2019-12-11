a3cc8deab56bc99a85d6a3b5357e5aff733117e8
mdrezaali1 <mdrezaali1@gmail.com>
2019-12-11 21:52:00 +0600

    first commit

---
+++ ruperbazar/atova/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ruperbazar/atova/');
+define('HTTP_CATALOG', 'http://localhost/ruperbazar/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ruperbazar/atova/');
+define('HTTPS_CATALOG', 'http://localhost/ruperbazar/');
+
+// DIR 
+define('DIR_APPLICATION', 'C:/xampp/htdocs/ruperbazar/atova/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/ruperbazar/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/ruperbazar/atova/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/ruperbazar/atova/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/ruperbazar/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/ruperbazar/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/ruperbazar/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/ruperbazar/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/ruperbazar/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/ruperbazar/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/ruperbazar/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/ruperbazar/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'rupperbuzer');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ ruperbazar/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ruperbazar/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ruperbazar/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/ruperbazar/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/ruperbazar/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/ruperbazar/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/ruperbazar/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/ruperbazar/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/ruperbazar/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/ruperbazar/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/ruperbazar/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/ruperbazar/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/ruperbazar/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/ruperbazar/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'rupperbuzer');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
